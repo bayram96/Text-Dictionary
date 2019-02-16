0d493a9c4944c8cf9f0411d7c37b6026eb7d8886
Bayramgeldi <bnurberdiyev@colotek.com>
2019-02-15 19:57:16 -0500

    Updates

---
+++ precache-manifest.d9a1329ca400cb833ae40869a31fce85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/texting-dictionary/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2919b8092201d0c4ce57",
+    "url": "/texting-dictionary/static/js/runtime~main.2919b809.js"
+  },
+  {
+    "revision": "0fbf0177c2777a235aca",
+    "url": "/texting-dictionary/static/js/main.0fbf0177.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/texting-dictionary/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "0fbf0177c2777a235aca",
+    "url": "/texting-dictionary/static/css/main.1b8e64be.chunk.css"
+  },
+  {
+    "revision": "d73a2a9a4bb911a70194382e1a193872",
+    "url": "/texting-dictionary/index.html"
+  }
+];
